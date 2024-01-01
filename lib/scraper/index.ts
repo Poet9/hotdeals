4432da1bf5ab9b0dd2b472a279d08dc99d70ad0b
poet9 <baminoo19@gmail.com>
2024-01-01 17:14:26 +0100

    using puppeteer for fetching instead of axious

--- lib/scraper/index.ts
+++ lib/scraper/index.ts
@@ -1,17 +1,17 @@
-import * as cheerio from "cheerio";
+import puppeteer from "puppeteer";
 export async function scrapeAEProduct(productUrl: string) {
-    if (!productUrl) return null;
-    try {
-        const response = await fetch(productUrl, {
-            method: "GET",
-            credentials: "include",
-            mode: "cors",
-        });
-        const data = await response.text();
-        const $ = cheerio.load(data);
-        const title = $('h1[data-pl="product-title"]').text();
-        console.log({ title });
-    } catch (err: any) {
-        console.log(`Failed to scrape product ${err.message}`);
-    }
+  if (!productUrl) return null;
+  const browser: any = await puppeteer.launch({ headless: false });
+  try {
+    const page: any = await browser.newPage();
+    await page.goto(productUrl);
+    // Query for an element handle.
+    const element = await page.waitForSelector('h1[data-pl="product-title"]');
+    const title = await element.evaluate((el: any) => el.textContent);
+    console.log({ title });
+  } catch (err: any) {
+    console.log(`Failed to scrape product ${err.message}`);
+  } finally {
+    await browser.close();
+  }
 }
