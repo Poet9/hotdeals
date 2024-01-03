4a1790e0c871bcc7424f60defaaa32e1a079fe86
Amine <75739071+Poet9@users.noreply.github.com>
2024-01-03 17:40:40 +0100

    rebasing the commit tree

--- lib/scraper/index.ts
+++ lib/scraper/index.ts
@@ -1,17 +1,27 @@
 import * as cheerio from "cheerio";
+import axios from "axios";
+
 export async function scrapeAEProduct(productUrl: string) {
     if (!productUrl) return null;
+    const options = {
+        auth: {
+            username: `${String(process.env.BRIGHT_DATA_USERNAME)}-session-${
+                (1000000 * Math.random()) | 0
+            }`,
+            password: String(process.env.BRIGHT_DATA_PASSWORD),
+        },
+        host: "brd.superproxy.io",
+        port: 22225,
+        rejectUnauthorized: false,
+    };
     try {
-        const response = await fetch(productUrl, {
-            method: "GET",
-            credentials: "include",
-            mode: "cors",
-        });
-        const data = await response.text();
-        const $ = cheerio.load(data);
-        const title = $('h1[data-pl="product-title"]').text();
+        const response = await axios.get(productUrl, options);
+        const $ = cheerio.load(response.data);
+        const title = $(".magnifier--image--L4hZ4dC").attr();
         console.log({ title });
     } catch (err: any) {
         console.log(`Failed to scrape product ${err.message}`);
     }
 }
+
+// title--wrap--Ms9Zv4A
