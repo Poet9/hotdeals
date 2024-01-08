1a7616e72622e5357cb51bad4ca37e4f4fbd3a44
Amine <75739071+Poet9@users.noreply.github.com>
2024-01-08 09:24:41 +0100

    solved merge conflict

--- lib/scraper/index.ts
+++ lib/scraper/index.ts
@@ -1,17 +1,17 @@
 import puppeteer from "puppeteer";
 export async function scrapeAEProduct(productUrl: string) {
-  if (!productUrl) return null;
-  const browser: any = await puppeteer.launch({ headless: false });
-  try {
-    const page: any = await browser.newPage();
-    await page.goto(productUrl);
-    // Query for an element handle.
-    const element = await page.waitForSelector('h1[data-pl="product-title"]');
-    const title = await element.evaluate((el: any) => el.textContent);
-    console.log({ title });
-  } catch (err: any) {
-    console.log(`Failed to scrape product ${err.message}`);
-  } finally {
-    await browser.close();
-  }
+    if (!productUrl) return null;
+    const browser: any = await puppeteer.launch({ headless: false });
+    try {
+        const page: any = await browser.newPage();
+        await page.goto(productUrl);
+        // Query for an element handle.
+        const element = await page.waitForSelector('h1[data-pl="product-title"]');
+        const title = await element.evaluate((el: any) => el.textContent);
+        console.log({ title });
+    } catch (err: any) {
+        console.log(`Failed to scrape product ${err.message}`);
+    } finally {
+        await browser.close();
+    }
 }
