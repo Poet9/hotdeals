8cb07c9651be16678d4547d8fb9424ac0781fbf5
Amine <75739071+Poet9@users.noreply.github.com>
2024-01-10 15:25:09 +0100

    setting up the db for storage

--- lib/actions/index.ts
+++ lib/actions/index.ts
@@ -6,6 +6,7 @@ export async function scrapeAndStoreProduct(productUrl: string) {
     if (!productUrl) return null;
     try {
         const data = await scrapeAEProduct(productUrl);
+        console.log({ data });
     } catch (err: any) {
         console.log(`Failed to create/update product ${err.message}`);
     }
--- lib/scraper/index.ts
+++ lib/scraper/index.ts
@@ -8,9 +8,10 @@ export async function scrapeAEProduct(productUrl: string) {
     });
 
     const page: any = await browser.newPage();
-
+    var productData: itemClient | null = null;
     try {
-        await page.goto(productUrl, { waitUntil: "networkidle2" });
+        await page.goto(productUrl, { waitUntil: "networkidle2", timeout: 60000 });
+        // changing currency to be always in euro (or the first suggested currency for that matter)
         await page.$eval(
             "#gdpr-new-container",
             (el: HTMLDivElement) => (el.style.display = "none")
@@ -27,37 +28,25 @@ export async function scrapeAEProduct(productUrl: string) {
         );
         await page.click(".es--saveBtn--w8EuBuy");
         await page.click(".es--saveBtn--w8EuBuy");
-
-        // // helper function
-        // const getPrice = (spans: any): string => {
-        //     let price: string = "";
-        //     for (const span of spans) {
-        //         price += span.textContent;
-        //     }
-        //     return price;
-        // };
         // starting the scraping functionality
         await page.reload({ waitUntil: "load" });
         await page.waitForSelector(".pdp-info");
         const productContainer = await page.$(".pdp-info");
         if (!productContainer) throw new Error("the page did not load correctly");
-        const productData: itemClient = await page.evaluate(
+        productData = await page.evaluate(
             (el: Element) => ({
                 title: el.querySelector('h1[data-pl="product-title"]')?.textContent,
-                image: el.querySelector(".magnifier--image--L4hZ4dC")?.getAttribute("src") || "",
+                image: el.querySelector(".magnifier--image--L4hZ4dC")?.getAttribute("src"),
                 url: "",
                 currentPrice: "0.0000",
-                originalPrice:
-                    el.querySelector(".price--originalText--Zsc6sMv")?.textContent || "000.00",
-                discount: el.querySelector(".price--discount--xET8qnP")?.textContent || "",
-                reviewCount:
-                    el
-                        .querySelector('div[data-pl="product-reviewer"]')
-                        ?.lastChild?.textContent?.trim() || "000",
-                stars:
-                    el
-                        .querySelector('div[data-pl="product-reviewer"] > strong')
-                        ?.textContent?.trim() || "0000",
+                originalPrice: el.querySelector(".price--originalText--Zsc6sMv")?.textContent,
+                discount: el.querySelector(".price--discount--xET8qnP")?.textContent,
+                reviewCount: el
+                    .querySelector('div[data-pl="product-reviewer"]')
+                    ?.lastChild?.textContent?.trim(),
+                stars: el
+                    .querySelector('div[data-pl="product-reviewer"] > strong')
+                    ?.textContent?.trim(),
                 inStock: true,
             }),
             productContainer
@@ -66,13 +55,14 @@ export async function scrapeAEProduct(productUrl: string) {
             ".es--wrap--erdmPRe > span",
             (digits: HTMLSpanElement[]) => digits.map((digit) => digit.textContent)
         );
-        productData.currentPrice = priceArr.join("");
-        productData.url = productUrl;
-        // I have to send the data to be saved in the database
-        console.log(productData);
+        if (productData) {
+            productData.currentPrice = priceArr.join("");
+            productData.url = productUrl;
+        }
     } catch (err: any) {
         console.log(`Failed to scrape product ${err.message}`);
     } finally {
         await browser.close();
+        return productData; // sending data to be saved in the database
     }
 }
