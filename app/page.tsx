b803a158d0aad650510f20f317dd8f60362a6ac5
poet9 <baminoo19@gmail.com>
2024-01-11 22:35:11 +0100

    setting up the UI

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,29 @@
+import Card from "@/components/card";
 import CarouselEl from "@/components/carousel";
 import Search from "@/components/search";
-
-export default function Home() {
-    return (
-        <main className="flex min-h-screen py-10 flex-col items-center justify-between">
-            <section className="xl:flex p-16">
-                <div className="">
-                    <h2 className="text-6xl">
-                        Buy what you need at the{" "}
-                        <span className="text-[#c59f]">right time with the right price</span>
-                    </h2>
-                    <Search />
-                </div>
-                <CarouselEl />
-            </section>
-        </main>
-    );
+import { getAllProducts } from "@/lib/actions";
+import { item } from "@/types";
+export default async function Home() {
+  const allProducts = await getAllProducts();
+  return (
+    <main className="flex min-h-screen py-10 flex-col items-center justify-between">
+      <section className="xl:flex p-16">
+        <div className="">
+          <h2 className="text-6xl">
+            Buy what you need at the{" "}
+            <span className="text-[#c59f]">
+              right time with the right price
+            </span>
+          </h2>
+          <Search />
+        </div>
+        <CarouselEl />
+      </section>
+      <section>
+        {allProducts?.map((product: item & { id: string }) => (
+          <Card key={product.id} product={product} />
+        ))}
+      </section>
+    </main>
+  );
 }
